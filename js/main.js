e2dde661c88c03dd6238655c6942914995428d3c
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-23 15:40:34 +0100

    con animazione

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 var openMenu = $(".header-right > a");
 openMenu.click(
     function() {
-        $(".hamburger-menu").addClass("active");
+        // $(".hamburger-menu").addClass("active");
+        $(".hamburger-menu").fadeIn("slow");
     }
 );
 
 var closeMenu = $("a.close");
 closeMenu.click(
     function() {
-        $(".hamburger-menu").removeClass("active");
+        // $(".hamburger-menu").removeClass("active");
+        $(".hamburger-menu").fadeOut("slow");
     }
 );
